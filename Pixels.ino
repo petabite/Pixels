58339b2f733a63ae3f8a7c82360157e4d00a81d5
petabite <jmslca123@gmail.com>
2020-06-07 21:10:01 -0700

    add on/off toggle interrupt

--- Pixels.ino
+++ Pixels.ino
@@ -5,18 +5,32 @@
 #define NUM_LEDS 30
 #define PIN 10
 
+boolean on;
 byte current_animation;
 void setup() {
-  // Serial.begin(9600);
-  // while (!Serial);
   // init Pixels
   CRGB leds[NUM_LEDS];
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
   Pixels pixels(leds, NUM_LEDS);
-
+  // get on/off byte from EEPROM
+  on = EEPROM.read(1);
+  // get current animation from EEPROM
+  current_animation = EEPROM.read(0);
+  // init EEPROM if it's a new board
+  if (current_animation == 255)
+    current_animation = 0;
+    EEPROM.update(0, current_animation);
+  if (on == 255)
+    on = 1;
+    EEPROM.update(1, on);
+  // config interrupt pins
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), nextAnimation, FALLING);
-  current_animation = EEPROM.read(0);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(3), toggleOnOff, FALLING);
+  // don't do anything if off
+  while(!on);
+  // loop the selected animation
   switch (current_animation)
   {
   case 0:
@@ -73,24 +87,42 @@ void setup() {
   }
 }
 
+// interrupt vars
+static unsigned long last_interrupt_time = 0;
+unsigned long interrupt_time;
+#define INTERRUPT_DELAY 300
+
+void toggleOnOff() {
+  interrupt_time = millis();
+  // debounce the interrupt
+  if (interrupt_time - last_interrupt_time > INTERRUPT_DELAY)
+  {
+    on = !on;
+    //save to EEPROM
+    EEPROM.update(1, on);
+    // assembly to jump to beginning of code(basically like pressing reset button)
+    asm volatile ("  jmp 0");
+  }
+  last_interrupt_time = interrupt_time;
+}
+
 void nextAnimation() {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 300)
+  interrupt_time = millis();
+  // debounce the interrupt
+  if (interrupt_time - last_interrupt_time > INTERRUPT_DELAY)
   {
     current_animation++;
-    //reset saved animation if past total
+    //reset saved animation if past max
     if (current_animation > 12)
       EEPROM.update(0, 0);
     else
       EEPROM.update(0, current_animation);
+    // assembly to jump to beginning of code(basically like pressing reset button)
     asm volatile ("  jmp 0");
   }
   last_interrupt_time = interrupt_time;
 }
 
 void loop() {
-  // pixels->chase(20, CRGB(255,0,0), LEFT);
-  // pixels->chase(20, CRGB(255,0,0), LEFT);
+
 }
